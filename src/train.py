34aa84191d73db13b105cc5f7726c5c40a90947a
Bramantyo Adrian <bramantyo.adrian@bram.local>
2018-05-21 16:14:08 +0700

    fix batch and step size

--- src/train.py
+++ src/train.py
@@ -99,10 +99,10 @@ class InceptionV4Trainer(Trainer):
         callbacks = self.get_callback(model_checkpoint_file='data/model/inception_v4_model_checkpoint.hdf5',
                                       log_file='logs/inception_v4_keras_log.csv',
                                       tensorboard_dir='data/inception_v4_tensorboard/')
-        self.create_train_validation_set(56)
+        self.create_train_validation_set(100)
         self.create_model()
         self.model.fit_generator(self.train_data_generator,
-                                 steps_per_epoch=126,
+                                 steps_per_epoch=70,
                                  epochs=200,
                                  validation_data=self.validation_data_generator,
                                  validation_steps=1,
